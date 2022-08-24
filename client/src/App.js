23f6e66415407750370ef15eb5fa2a6fad8bf60c
Yves Kipondo <yvesdaxmaz@gmail.com>
2022-08-24 05:48:54 +0200

    add Users page component

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
-import Layout from  './containers/Layout';
+import Layout from './containers/Layout';
 import Header from './components/Header/Header';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Users from './pages/Users/Users';
 
 function App() {
   return (
-      <Layout>
-        <Header />
-        <Routes>
-            <Route path="/" element={<Home />} />
-        </Routes>
-      </Layout>
+    <Layout>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
+    </Layout>
   );
 }
 
---
+++ client/src/pages/Users/Users.js
@@ -0,0 +1,16 @@
+const Users = props => {
+  return (
+    <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="bg-white overflow-hidden shadow rounded-lg divide-y divide-gray-200">
+        <div className="px-4 py-5 sm:px-6">
+          <h3 className="text-lg leading-6 font-medium text-gray-900">
+            All Users
+          </h3>
+        </div>
+        <div className="px-4 py-5 sm:p-6"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Users;
