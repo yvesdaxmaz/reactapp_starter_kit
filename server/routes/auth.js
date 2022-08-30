d1562169ca52c53c73124e539eb98951b7bb0ad4
Yves Kipondo <yvesdaxmaz@gmail.com>
2022-08-30 23:32:34 +0200

    Add /auth/signin_with_token route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const router = express.Router();
 const db = require('./../models');
+const passport = require('passport');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -11,6 +12,48 @@ router.get('/', function(req, res, next) {
     res.status(200).json(users);
   });
 });
+
+/* GET check user availability */
+router.get(
+  '/signin_with_token',
+  (req, res, next) =>
+    passport.authenticate('jwt', { session: false }, function(err, user, info) {
+      if (err || !user) {
+        res.status(404).json({
+          type: 'Not Found',
+          message: 'Unable to authenticate user with provided token.',
+        });
+      } else {
+        return next();
+      }
+    })(req, res, next),
+  function(req, res, next) {
+    let authorization = req.headers.authorization;
+    let jwt_token = authorization.split(' ')[1];
+    jwt.verify(
+      jwt_token,
+      '5ebe2294ecd0e0f08eab7690d2a6ee69',
+      (err, payload) => {
+        db.User.findByPk(payload.id).then(user => {
+          if (user) {
+            let token = jwt.sign(
+              { id: user.id },
+              '5ebe2294ecd0e0f08eab7690d2a6ee69',
+            );
+            delete user.password;
+            res.status(200).json({ token, user });
+          } else {
+            res.status(404).json({
+              code: 404,
+              type: 'Not Found',
+              message: 'Unable to authenticate user with provided token.',
+            });
+          }
+        });
+      },
+    );
+  },
+);
 
 /* POST create a user. */
 router.post('/signin', function(req, res, next) {
--- server/tests/auth.js
+++ server/tests/auth.js
@@ -5,6 +5,7 @@ var chaiHttp = require('chai-http');
 var expect = require('chai').expect;
 var app = require('../app');
 var db = require('./../models');
+var jwt = require('jsonwebtoken');
 
 chai.should();
 chai.use(chaiHttp);
@@ -99,6 +100,47 @@ describe('Api auth routes', () => {
           res.body.message.should.be.eql('Email or password is incorrect');
           done();
         });
+    });
+  });
+
+  describe.only('GET route /auth/signin_with_token', () => {
+    it('should not authenticate user without valid token', done => {
+      chai
+        .request(app)
+        .get('/auth/signin_with_token')
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.type.should.be.eql('Not Found');
+          res.body.message.should.be.eql(
+            'Unable to authenticate user with provided token.',
+          );
+          done();
+        });
+    });
+
+    it('should not authenticate user without valid token', done => {
+      db.User.create({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456',
+      }).then(user => {
+        let token = jwt.sign(
+          { id: user.id },
+          '5ebe2294ecd0e0f08eab7690d2a6ee69',
+        );
+        chai
+          .request(app)
+          .get('/auth/signin_with_token')
+          .set('Authorization', `Bearer ${token}`)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('token');
+            res.body.user.should.be.a('object');
+            res.body.user.email.should.be.eql('johndoe@example.com');
+            done();
+          });
+      });
     });
   });
 });
